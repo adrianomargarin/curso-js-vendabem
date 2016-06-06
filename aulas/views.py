96c2def61605d5c55011b86060e7ee0b7211258e
Adriano Margarin <adriano.margarin@gmail.com>
2016-06-06 15:47:28 -0300

    Home

--- aulas/views.py
+++ aulas/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+
+
+class Home(TemplateView):
+
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        import cursojs.urls as project_urls
+
+        context = super(Home, self).get_context_data(**kwargs)
+
+        urls = project_urls.urlpatterns.copy()
+        urls.pop(0)
+        urls = [url.name for url in urls]
+
+        context['urls'] = urls
+
+        return context
--- cursojs/urls.py
+++ cursojs/urls.py
@@ -16,39 +16,38 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
-
+from aulas.views import Home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^aula-01/$', TemplateView.as_view(template_name="aulas/index.html"),
         name='aula-01'),
-    url(r'^aula-01/bootstrap$',
+    url(r'^aula-01/bootstrap/$',
         TemplateView.as_view(template_name="aulas/index-bootstrap.html"),
         name='aula-01-bootstrap'),
 
-    url(r'^aula-02/example-js$',
+    url(r'^aula-02/example-js/$',
         TemplateView.as_view(template_name="aulas/example-js.html"),
         name='example-1-js'),
-    url(r'^aula-02/example-2-js$',
+    url(r'^aula-02/example-2-js/$',
         TemplateView.as_view(template_name="aulas/example-2-js.html"),
         name='example-2-js'),
-    url(r'^aula-02/example-3-js$',
+    url(r'^aula-02/example-3-js/$',
         TemplateView.as_view(template_name="aulas/example-3-js.html"),
         name='example-3-js'),
-    url(r'^aula-02/example-4-js$',
+    url(r'^aula-02/example-4-js/$',
         TemplateView.as_view(template_name="aulas/example-4-js.html"),
         name='example-4-js'),
-    url(r'^aula-02/commands$',
+    url(r'^aula-02/commands/$',
         TemplateView.as_view(template_name="aulas/commands.html"),
         name='commands'),
-    url(r'^aula-02/debug-js$',
+    url(r'^aula-02/debug-js/$',
         TemplateView.as_view(template_name="aulas/debug-js.html"),
         name='debug-js'),
-    url(r'^aula-02/exercises$',
+    url(r'^aula-02/exercises/$',
         TemplateView.as_view(template_name="aulas/exercises.html"),
         name='exercises'),
 
-    url(r'^$', TemplateView.as_view(template_name="index.html"),
-        name='index'),
+    url(r'^$', Home.as_view(), name='index'),
 ]
